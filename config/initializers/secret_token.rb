c20ed64c1f358a88f2c0004cd2317615105c2b6b
Derrick Nguyen <dbln79@gmail.com>
2012-02-24 13:19:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobswept::Application.config.secret_token = '0e52a71a3feec183c76371ca4c7c34f1b40741e45463e7a618844418cb9b995366c0ffd263f34e10bed881258029a45053b71a3243abd9ecdebc3d26e315cc7c'
